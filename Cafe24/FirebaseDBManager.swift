d8047eec72d7100e1554061041bbd235da8fa7e6
bhpark <rhehd721@gmail.com>
2025-06-15 23:20:26 +0900

    feat: add LaunchScreen & Firebase Package install
    - 로고 이미지 추가
    - LaunchScreen 추가
    - Firebase Package 설치 (연동전)

---
+++ Cafe24/FirebaseDBManager.swift
@@ -0,0 +1,171 @@
+//
+//  FirebaseDBManager.swift
+//  Cafe24
+//
+//  Created by 박병호 on 6/15/25.
+//
+
+import Foundation
+import FirebaseDatabase
+
+struct StoreInfo : Codable, Identifiable, Hashable {
+    var id: String
+    var address: String
+    var group: String
+    var internet: String
+    var latitude: String
+    var longitude: String
+    var name: String
+    var number: String
+    var parking: String
+    var table: String
+    var toilet: String
+    var type: String
+    var date: String
+}
+
+class FirebaseDBManager: ObservableObject {
+    @Published var storeInfoList: [StoreInfo] = []
+    @Published var changeCount: Int = 0
+    
+    let ref: DatabaseReference? = Database.database().reference() // (1)
+    
+    private let encoder = JSONEncoder() // (2)
+    private let decoder = JSONDecoder() // (2)
+    
+    func listenToRealtimeDatabase() {
+        print("[FirebaseDBManager] listenToRealtimeDatabase")
+        
+        guard let databasePath = ref?.child("storeInfoList") else {
+            return
+        }
+        
+        databasePath
+            .observe(.childAdded) { [weak self] snapshot, _ in
+                guard
+                    let self = self,
+                    let json = snapshot.value as? [String: Any]
+                else {
+                    return
+                }
+                do {
+                    let storeData = try JSONSerialization.data(withJSONObject: json)
+                    let store = try self.decoder.decode(StoreInfo.self, from: storeData)
+                    self.storeInfoList.append(store)
+                } catch {
+                    print("an error occurred", error)
+                }
+            }
+        
+        databasePath
+            .observe(.childChanged){[weak self] snapshot, _ in
+                guard
+                    let self = self,
+                    let json = snapshot.value as? [String: Any]
+                else{
+                    return
+                }
+                do{
+                    let storeData = try JSONSerialization.data(withJSONObject: json)
+                    let store = try self.decoder.decode(StoreInfo.self, from: storeData)
+                    
+                    var index = 0
+                    for storeItem in self.storeInfoList {
+                        if (store.id == storeItem.id){
+                            break
+                        }else{
+                            index += 1
+                        }
+                    }
+                    self.storeInfoList[index] = store
+                } catch{
+                    print("an error occurred", error)
+                }
+            }
+        
+        databasePath
+            .observe(.childRemoved){[weak self] snapshot in
+                guard
+                    let self = self,
+                    let json = snapshot.value as? [String: Any]
+                else{
+                    return
+                }
+                do{
+                    let storeData = try JSONSerialization.data(withJSONObject: json)
+                    let store = try self.decoder.decode(StoreInfo.self, from: storeData)
+                    for (index, storeItem) in self.storeInfoList.enumerated() where store.id == storeItem.id {
+                        self.storeInfoList.remove(at: index)
+                    }
+                } catch{
+                    print("an error occurred", error)
+                }
+            }
+        
+        databasePath
+            .observe(.value){[weak self] snapshot in
+                guard
+                    let self = self
+                else {
+                    return
+                }
+                self.changeCount += 1
+            }
+    }
+    
+    func stopListening() {
+        print("[FirebaseDBManager] stopListening")
+        
+        ref?.removeAllObservers()
+    }
+    
+    func addNewStore(storeInfo: StoreInfo) {
+        print("[FirebaseDBManager] addNewStore")
+        
+        self.ref?.child("storeInfoList").child("\(storeInfo.id)").setValue([
+            "id": storeInfo.id,
+            "address": storeInfo.address,
+            "group": storeInfo.group,
+            "internet": storeInfo.internet,
+            "latitude": storeInfo.latitude,
+            "longitude": storeInfo.longitude,
+            "name": storeInfo.name,
+            "number": storeInfo.number,
+            "parking": storeInfo.parking,
+            "table": storeInfo.table,
+            "toilet": storeInfo.toilet,
+            "type": storeInfo.type,
+            "date": storeInfo.date
+        ])
+    }
+    
+    func deleteStore(key: String) {
+        ref?.child("storeInfoList/\(key)").removeValue()
+    }
+    
+    func editStore(storeInfo: StoreInfo) {
+        let updates: [String : Any] = [
+            "id": storeInfo.id,
+            "address": storeInfo.address,
+            "group": storeInfo.group,
+            "internet": storeInfo.internet,
+            "latitude": storeInfo.latitude,
+            "longitude": storeInfo.longitude,
+            "name": storeInfo.name,
+            "number": storeInfo.number,
+            "parking": storeInfo.parking,
+            "table": storeInfo.table,
+            "toilet": storeInfo.toilet,
+            "type": storeInfo.type,
+            "date": storeInfo.date
+        ]
+        
+        let childUpdates = ["storeInfoList/\(storeInfo.id)": updates]
+        for (index, storeItem) in storeInfoList.enumerated() where storeItem.id == storeInfo.id {
+            storeInfoList[index] = storeInfo
+        }
+        self.ref?.updateChildValues(childUpdates)
+        
+    }
+    
+}
---
+++ Cafe24/View/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  Cafe24
+//
+//  Created by 박병호 on 6/15/25.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Image("LaunchImage")
+    }
+}
+
+#Preview {
+    LaunchView()
+}
